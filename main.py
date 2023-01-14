e222b98c9b4f805a6abf035c6cec0bf4efb59e3d
Mara Badea <badeamara2606@gmail.com>
2023-01-14 18:54:48 +0200

    refacut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import numpy as np
 from sklearn import svm, metrics
-from sklearn.model_selection import train_test_split
-
 import os
 
 file_path=os.getcwd()
@@ -81,7 +79,13 @@ nephritis=np.array(nephritis)
 m = np.array([temperature, nausea, l_pain, urine, m_pains, burning, inflamation_u, nephritis])
 
 m = m.T
-X_train, X_test, Y_train, Y_test = train_test_split(m[:,:6], m[:,6:], test_size=0.25)
+x=m[:,:6]
+y=m[:,6:]
+
+X_train=x[:90,:]
+X_test=x[90:, :]
+Y_train=y[:90,:]
+Y_test=y[90: , :]
 
 inflam_train = Y_train[:, 0]
 nephr_train = Y_train[:, 1]
@@ -102,3 +106,4 @@ for i in range(-5, 9, 2):
     predictie = clf.predict(X_test)
     acuratete_Z = metrics.accuracy_score(y_true=nephr_test, y_pred=predictie)
     print("Acuratetea pentru nephritis of renal pelvis origin pentru costul", 2 ** i, "este", acuratete_Z)
+
